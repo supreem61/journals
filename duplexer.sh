1e3c678b4ca39dd2d0510e3bc50348c8d5d321d7
Gaurav Juvekar <gauravjuvekar@gmail.com>
2013-07-29 10:27:53 +0530

    Add physics

--- duplexer.sh
+++ duplexer.sh
@@ -5,21 +5,22 @@ read $string
 
 count=1
 
-for odd in odd*
+for odd in `ls odd* |sort -V`
 	do
 		mv $odd page-"$count$string"
-		count=`expr $count + 2`
+		count=`expr $count + 2`		
 	done
 
 count=0
 
-for even in even*
+for even in `ls even*|sort -V`
 	do
 		count=`expr $count + 2`
 	done
 	
-for even in even*
+for even in `ls even*|sort -V`
 	do
 		mv $even page-"$count$string"
 		count=`expr $count - 2`
 	done
+read $string
